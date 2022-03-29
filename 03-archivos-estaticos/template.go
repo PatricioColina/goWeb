4ce3a32b9f1eec1c2fb2d8d7a6cda9ddde81b633
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-29 09:21:18 -0300

    manejo archivos estaticos

---
+++ 03-archivos-estaticos/template.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type Usuarios struct {
+	UserName string
+	Edad     int
+	Activo   bool
+	Admin    bool
+	Cursos   []Curso
+}
+
+type Curso struct {
+	Nombre string
+}
+
+func Saludar(nombre string) string {
+	return "Hola " + nombre + " desde una funcion"
+}
+
+var templates = template.Must(template.New("T").ParseGlob("templates/**/*.html")) //de tener mas paginas se agregan aca
+var errorTemplate = template.Must(template.ParseFiles("templates/error/error.html"))
+
+func handlerError(rw http.ResponseWriter, status int) {
+	rw.WriteHeader(status)
+	errorTemplate.Execute(rw, nil)
+}
+
+func renderTemplate(rw http.ResponseWriter, name string, data interface{}) {
+	err := templates.ExecuteTemplate(rw, name, data)
+
+	if err != nil {
+		//http.Error(rw, "No es posible retornar el template.", http.StatusInternalServerError)
+		handlerError(rw, http.StatusInternalServerError)
+	}
+}
+
+func Index(rw http.ResponseWriter, r *http.Request) {
+
+	c1 := Curso{"GO"}
+	c2 := Curso{"Python"}
+	c3 := Curso{"HTML"}
+	c4 := Curso{"JAVA"}
+	c5 := Curso{"C#"}
+
+	//fmt.Fprintln(rw, " hola mundo")
+
+	//template, err := template.ParseFiles("index.html", "base.html")
+	//
+	/*funciones := template.FuncMap{
+		"saludar": Saludar,
+	}*/
+	//template, err := template.New("index.html").Funcs(funciones).ParseFiles("index.html")
+
+	cursos := []Curso{c1, c2, c3, c4, c5}
+
+	usuario := Usuarios{"patricio", 36, true, false, cursos}
+
+	//template.Execute(rw, nil)
+	//template.Execute(rw, usuario)
+	/*err := templates.ExecuteTemplate(rw, "index.html", usuario)
+
+	if err != nil {
+		panic(err)
+	}*/
+	renderTemplate(rw, "index.html", usuario)
+
+}
+
+func Registro(rw http.ResponseWriter, r *http.Request) {
+	renderTemplate(rw, "registro.html", nil)
+}
+
+func main() {
+	//archivos estaticos
+	staticFile := http.FileServer(http.Dir("static"))
+
+	//mux
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+	mux.HandleFunc("/registro", Registro)
+	mux.Handle("/static/", http.StripPrefix("/static/", staticFile))
+
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+
+	fmt.Println("El servidot esta conectado al puerto 3000")
+	fmt.Println("server: http://localhost:3000/")
+	log.Fatal(server.ListenAndServe())
+
+}
