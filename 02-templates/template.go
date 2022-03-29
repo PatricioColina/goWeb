d41c6a66a861d08955c8343f4476cff8cb716c04
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-28 21:39:56 -0300

    Bucles

--- 02-templates/template.go
+++ 02-templates/template.go
@@ -12,12 +12,26 @@ type Usuarios struct {
 	Edad     int
 	Activo   bool
 	Admin    bool
+	Cursos   []Curso
+}
+
+type Curso struct {
+	Nombre string
 }
 
 func Index(rw http.ResponseWriter, r *http.Request) {
+	c1 := Curso{"GO"}
+	c2 := Curso{"Python"}
+	c3 := Curso{"HTML"}
+	c4 := Curso{"JAVA"}
+	c5 := Curso{"C#"}
+
 	//fmt.Fprintln(rw, " hola mundo")
 	template, err := template.ParseFiles("index.html")
-	usuario := Usuarios{"patricio", 36, true, false}
+
+	cursos := []Curso{c1, c2, c3, c4, c5}
+
+	usuario := Usuarios{"patricio", 36, true, false, cursos}
 
 	if err != nil {
 		panic(err)
