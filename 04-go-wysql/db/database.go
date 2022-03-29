b579852e046c05da7dc526f6081098b5ef69827e
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-29 13:19:30 -0300

    conexion a mysql

--- 04-go-wysql/db/database.go
+++ 04-go-wysql/db/database.go
@@ -2,27 +2,40 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 //generacion de conexion
 //username:password@tcp(localhost:3306)/database
-const url = "root;root@tcp(localhost:3306)/goWeb_db"
+const url = "root:root@tcp(localhost:3306)/goweb_db"
 
+//db, err := sql.Open("mysql", "user:password@/dbname")
+
+//guarda la conexion
 var db *sql.DB
 
+//realiza la conexion
 func Connect() {
 	conection, err := sql.Open("mysql", url)
 
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Println("conexion exitosa")
 	db = conection
 
 }
 
+//cierra la conexion
 func Close() {
 	db.Close()
+}
+
+//verificar la conexion
+func Ping() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
 }
