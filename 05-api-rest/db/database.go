a5e13113b611d4cc409a1ba2739249d923d4712b
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-29 17:55:29 -0300

    nuevo entorno de trabajo

---
+++ 05-api-rest/db/database.go
@@ -0,0 +1,88 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//generacion de conexion
+//username:password@tcp(localhost:3306)/database
+const url = "root:root@tcp(localhost:3306)/goweb_db"
+
+//db, err := sql.Open("mysql", "user:password@/dbname")
+
+//guarda la conexion
+var db *sql.DB
+
+//realiza la conexion
+func Connect() {
+	conection, err := sql.Open("mysql", url)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("conexion exitosa")
+	db = conection
+
+}
+
+//cierra la conexion
+func Close() {
+	db.Close()
+}
+
+//verificar la conexion
+func Ping() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
+
+//CREA UNA TABLA
+func CreateTable(schema string, name string) {
+	if !ExitsTable(name) {
+		_, err := db.Exec(schema)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
+
+}
+
+//reiniciar tabla
+func TruncateTable(tableName string) {
+	sql := fmt.Sprintf("TRUNCATE TABLE %s", tableName)
+	Exec(sql)
+}
+
+//funcion que valida la tabla
+func ExitsTable(tableName string) bool {
+	sql := fmt.Sprintf("SHOW TABLE LIKE '%s'", tableName)
+	rows, err := db.Query(sql)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	return rows.Next()
+}
+
+func Exec(query string, args ...interface{}) (sql.Result, error) {
+	resust, err := db.Exec(query, args...)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return resust, err
+}
+
+func Query(query string, args ...interface{}) (*sql.Rows, error) {
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	return rows, err
+}
---
+++ 05-api-rest/models/users.go
@@ -0,0 +1,93 @@
+package models
+
+import "apirest/db"
+
+type User struct {
+	Id       int64
+	UserName string
+	Password string
+	Email    string
+}
+
+type Users []User
+
+const UserSchame string = `CREATE TABLE users (
+	id int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	usrname VARCHAR(30) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(50),
+	create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP )`
+
+//construir usuario
+func NewUsers(username, password, email string) *User {
+	user := &User{UserName: username, Password: password, Email: email}
+
+	return user
+}
+
+//crear usuario e insertar db
+func CreateUsers(username, password, email string) *User {
+	user := NewUsers(username, password, email)
+	user.insert()
+	return user
+}
+
+//inserta registro
+func (user *User) insert() {
+	sql := "INSERT users SET usrname=?,password=?,email=?"
+	result, _ := db.Exec(sql, user.UserName, user.Password, user.Email)
+
+	user.Id, _ = result.LastInsertId()
+
+}
+
+//listar registros
+func ListUsers() Users {
+	sql := "SELECT id, usrname,password, email FROM users"
+	users := Users{}
+
+	rows, _ := db.Query(sql)
+
+	for rows.Next() {
+		user := User{}
+		rows.Scan(&user.Id, &user.UserName, &user.Password, &user.Email)
+		users = append(users, user)
+	}
+
+	return users
+}
+
+//obtener un registro
+func GetUser(id int) *User {
+	user := NewUsers("", "", "")
+	sql := "SELECT id, usrname,password, email FROM users WHERE id=?"
+	rows, _ := db.Query(sql, id)
+	for rows.Next() {
+		rows.Scan(&user.Id, &user.UserName, &user.Password, &user.Email)
+	}
+
+	return user
+}
+
+//editar registro
+func (user *User) update() {
+	sql := "UPDATE users SET usrname=?,password=?,email=? WHERE id=?"
+
+	db.Exec(sql, user.UserName, user.Password, user.Email, user.Id)
+
+}
+
+//guardar o editar registro
+func (user *User) Save() {
+	if user.Id == 0 {
+		user.insert()
+	} else {
+		user.update()
+	}
+}
+
+//eliminar registro
+func (user *User) Delete() {
+	sql := "DELETE FROM users WHERE id=?"
+	db.Exec(sql, user.Id)
+}
