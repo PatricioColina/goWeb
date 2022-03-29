985bf5d2115ae21960c34a2d0e919b7548796081
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-28 21:45:13 -0300

    saluda desde funcion

--- 02-templates/template.go
+++ 02-templates/template.go
@@ -19,25 +19,36 @@ type Curso struct {
 	Nombre string
 }
 
+func Saludar() string {
+	return "Hola desde una funcion"
+}
+
 func Index(rw http.ResponseWriter, r *http.Request) {
-	c1 := Curso{"GO"}
-	c2 := Curso{"Python"}
-	c3 := Curso{"HTML"}
-	c4 := Curso{"JAVA"}
-	c5 := Curso{"C#"}
+	/*
+		c1 := Curso{"GO"}
+		c2 := Curso{"Python"}
+		c3 := Curso{"HTML"}
+		c4 := Curso{"JAVA"}
+		c5 := Curso{"C#"}
 
-	//fmt.Fprintln(rw, " hola mundo")
-	template, err := template.ParseFiles("index.html")
+		//fmt.Fprintln(rw, " hola mundo")
+	*/
 
-	cursos := []Curso{c1, c2, c3, c4, c5}
+	//template, err := template.ParseFiles("index.html")
+	funciones := template.FuncMap{
+		"saludar": Saludar,
+	}
+	template, err := template.New("index.html").Funcs(funciones).ParseFiles("index.html")
 
-	usuario := Usuarios{"patricio", 36, true, false, cursos}
+	//cursos := []Curso{c1, c2, c3, c4, c5}
+
+	//usuario := Usuarios{"patricio", 36, true, false, cursos}
 
 	if err != nil {
 		panic(err)
 	} else {
 		//template.Execute(rw, nil)
-		template.Execute(rw, usuario)
+		template.Execute(rw, nil)
 	}
 }
 
