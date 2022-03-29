498f804ccefcbbc0da9fdf6dbfc927db73764c5a
Patricio Colina <patricio.colina@externos.bci.cl>
2022-03-29 09:11:40 -0300

    manejo de error para navegacion

--- 02-templates/template.go
+++ 02-templates/template.go
@@ -24,12 +24,19 @@ func Saludar(nombre string) string {
 }
 
 var templates = template.Must(template.New("T").ParseGlob("templates/**/*.html")) //de tener mas paginas se agregan aca
+var errorTemplate = template.Must(template.ParseFiles("templates/error/error.html"))
+
+func handlerError(rw http.ResponseWriter, status int) {
+	rw.WriteHeader(status)
+	errorTemplate.Execute(rw, nil)
+}
 
 func renderTemplate(rw http.ResponseWriter, name string, data interface{}) {
 	err := templates.ExecuteTemplate(rw, name, data)
 
 	if err != nil {
-		panic(err)
+		//http.Error(rw, "No es posible retornar el template.", http.StatusInternalServerError)
+		handlerError(rw, http.StatusInternalServerError)
 	}
 }
 
